84bb45ac9af7c6ee0a79db6bb6f97792ee0a2ef0
MarcosLazarte <marcos.e.lazarte@gmail.com>
2022-11-12 18:15:26 -0300

    testing

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+        <ul>
+            <li>My Cuenta</li>
+            <li>Tienda</li>
+            <li>Carrito</li>
+            <li>Contactanos</li>
+            <li>Faqs</li>
+            <li>Términos de Privacidad</li>
+        </ul>
+        <h1>Kachaka</h1>
+    </>
+  )
+}
+
+export default Footer
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import Footer from '../../components/Footer/Footer'
+
+const Home = () => {
+  return (
+    <>
+    <div>
+        nav
+        con menu puede ser un component
+    </div>
+    <div>
+        presentacion
+        con imagenes
+    </div>
+    <div>
+        nuevos productos
+    </div>
+    <div>
+        categorias presentadas
+    </div>
+    <div>
+        market bello con algunas de las cosas
+    </div>
+    <div>
+        show now con imagen de fondo
+    </div>
+    <div>
+        imagen
+    </div>
+    <div>
+        news
+        sign up
+    </div>
+    <div>
+        menu del nav pero en un footer
+        puede ser un component
+    </div>
+    <div>
+        titulo en modo footer
+        puede ser un component
+    </div>
+    <Footer/>
+    </>
+  )
+}
+
+export default Home
